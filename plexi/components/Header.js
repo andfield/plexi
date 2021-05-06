0dd0bd6b835d60c523438546c18e4b91e5eb16e5
andfield <siddharththakur76@gmail.com>
2021-05-06 20:26:50 +1200

    Initial setup done

--- plexi/components/Header.js
+++ plexi/components/Header.js
@@ -13,21 +13,24 @@ function Header() {
   return (
     <div>
       <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
-        <Image
+        {/* <Image
           className="object-contain"
           src="https://links.papareact.com/ua6"
           width={200}
           height={100}
-        />
+        /> */}
+        <h1 className="text-6xl font-extrabold sm: mb-4 cursor-pointer">
+          PLEXI
+        </h1>
 
         {/* Header Button Container*/}
         <div className="flex flex-grow justify-evenly max-w-2xl">
-          <HeaderItem title="HOME" title="HOME" Icon={HomeIcon} />
-          <HeaderItem title="TRENDING" title="HOME" Icon={LightningBoltIcon} />
-          <HeaderItem title="VERIFIED" title="HOME" Icon={BadgeCheckIcon} />
-          <HeaderItem title="COLLECTIONS" title="HOME" Icon={CollectionIcon} />
-          <HeaderItem title="SEARCH" title="HOME" Icon={SearchIcon} />
-          <HeaderItem title="ACCOUNT" title="HOME" Icon={UserIcon} />
+          <HeaderItem title="HOME" Icon={HomeIcon} />
+          <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
+          <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
+          <HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
+          <HeaderItem title="SEARCH" Icon={SearchIcon} />
+          <HeaderItem title="ACCOUNT" Icon={UserIcon} />
         </div>
       </header>
     </div>
